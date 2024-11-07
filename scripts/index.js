02a128b8d83c8246f09387c308ec3c3e8cab094b
Natnael31 <end24002byui.edu>
2024-11-07 22:35:14 +0300

    html plus css

--- scripts/index.js
+++ scripts/index.js
@@ -16,3 +16,37 @@ ham.addEventListener('click', () => {
     list.classList.toggle('show');
     ham.classList.toggle('show');
 })
+
+// header way finding code
+const home = document.getElementById("home");
+const chamber = document.getElementById("chamber");
+const github = document.getElementById("github");
+const linkedIn = document.getElementById("linkedIn")
+
+home.addEventListener("click", () => {
+    home.classList.add('active');
+    chamber.classList.remove('active');
+    github.classList.remove('active');
+    linkedIn.classList.remove('active');
+})
+
+chamber.addEventListener("click", () => {
+    chamber.classList.add('active');
+    home.classList.remove('active');
+    github.classList.remove('active');
+    linkedIn.classList.remove('active');
+})
+
+github.addEventListener("click", () => {
+    github.classList.add('active');
+    home.classList.remove('active');
+    chamber.classList.remove('active');
+    linkedIn.classList.remove('active');
+})
+
+linkedIn.addEventListener("click", () => {
+    linkedIn.classList.add('active');
+    home.classList.remove('active');
+    chamber.classList.remove('active');
+    github.classList.remove('active');
+})
