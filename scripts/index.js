81ce2e2e6cc6a38d88b383330bd0c0d58acb799a
Natnael31 <end24002byui.edu>
2024-11-07 14:00:29 +0300

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+// Footer code
+let currentDate = new Date();
+let lastModified = new Date(document.lastModified);
+
+let year = document.getElementById("currentyear");
+year.textContent = `©${currentDate.getFullYear()}`;
+
+let date = document.getElementById("lastModified");
+date.textContent = `Last Modification : ${lastModified.getMonth() + 1}/${lastModified.getDate()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()}`
+
+// Burger code
+const ham = document.querySelector('nav');
+const list = document.getElementById("navigation");
+
+ham.addEventListener('click', () => {
+    list.classList.toggle('show');
+    ham.classList.toggle('show');
+})
