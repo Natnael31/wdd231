cb903910aa897520bcd78cb987eee55a808c6a91
Natnael31 <end24002byui.edu>
2024-11-14 21:53:36 +0300

    added prophets.html

---
+++ scripts/prophets.js
@@ -0,0 +1,48 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.getElementById('cards');
+
+async function getProphetData() {
+    // try {
+    //     var response;
+    //     fetch(url).then((res) => res.json()).then((data) => {
+    //         response = data;
+    //         console.log(response.prophets)
+    //     })
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.table(data.prophets);
+        // console.log(data.prophets)
+        displayProphets(data.prophets)
+    } catch (error) {
+        console.error();
+    }
+
+}
+
+getProphetData();
+
+// Display prophets
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
