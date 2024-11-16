3fc8b022ec39e483f6e4b2cbd0d79b952b615108
Natnael31 <end24002byui.edu>
2024-11-16 11:44:57 +0300

    chamber assignment

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,19 +1,97 @@
+// header code
+
+// Burger code
+const ham = document.querySelector('nav');
+const list = document.getElementById("navigation");
+
+ham.addEventListener('click', () => {
+    list.classList.toggle('show');
+    ham.classList.toggle('show');
+})
 
 
+// header way finding code
+const home = document.getElementById("home");
+const directory = document.getElementById("directory");
+const join = document.getElementById("join");
+const discover = document.getElementById("discover");
+const mainHeader = document.getElementById("main-header");
+
+home.classList.add('active');
+home.addEventListener("click", () => {
+    home.classList.add('active');
+    directory.classList.remove('active');
+    join.classList.remove('active');
+    discover.classList.remove('active');
+    mainHeader.textContent = "Home";
+})
+
+directory.addEventListener("click", () => {
+    directory.classList.add('active');
+    home.classList.remove('active');
+    join.classList.remove('active');
+    discover.classList.remove('active');
+    mainHeader.textContent = "Directory";
+})
+
+join.addEventListener("click", () => {
+    join.classList.add('active');
+    home.classList.remove('active');
+    directory.classList.remove('active');
+    discover.classList.remove('active');
+    mainHeader.textContent = "Join";
+})
+
+discover.addEventListener("click", () => {
+    discover.classList.add('active');
+    home.classList.remove('active');
+    directory.classList.remove('active');
+    join.classList.remove('active');
+    mainHeader.textContent = "Discover";
+})
 
 
+// Main code
+const cards = document.getElementById("cards");
+const url = "../chamber/data/memeber.json"
+
+async function getMemeberData() {
+    const memebers = await fetch(url);
+    const data = await memebers.json();
+    console.log(data);
+    displayMemebers(data);
+}
+
+getMemeberData();
+
+// Display memebers
+
+const displayMemebers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let businessName = document.createElement('h2');
+        let logo = document.createElement('img');
+        let birthDate = document.createElement('h3');
+        let placeOfBirth = document.createElement('h3');
 
 
+        businessName.textContent = `${member.name} ${member.lastname}`;
+        birthDate.textContent = member.birthdate;
+        placeOfBirth.textContent = member.birthplace;
+        logo.setAttribute('src', member.imageurl);
+        logo.setAttribute('alt', `logo of ${member.name} ${member.lastname}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '340');
+        logo.setAttribute('height', '440');
 
+        card.appendChild(businessName);
+        card.appendChild(birthDate);
+        card.appendChild(placeOfBirth);
+        card.appendChild(logo);
 
-
-
-
-
-
-
-
-
+        cards.appendChild(card);
+    });
+}
 
 
 // Footer code
