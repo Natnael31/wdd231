0faecf0e58e623b79df6537b8f75a6472f7f75bd
Natnael31 <end24002byui.edu>
2024-11-29 21:38:09 +0300

    completed chamber join page

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -53,33 +53,53 @@ discover.addEventListener("click", () => {
 
 
 // main code
+const url = window.location.href;
+const results = document.getElementById('data');
+// console.log(url);
 
-const timeStamp = document.getElementById('time-stamp');
-const currentDate = new Date();
-const dateSubmitted = currentDate.toLocaleDateString();
-const timeSubmitted = currentDate.toLocaleTimeString();
+const dataUrl = url.split('?');
+// console.log(dataUrl);
 
-timeStamp.value = `${dateSubmitted}, ${timeSubmitted}`;
-// console.log(timeStamp.value);
+formData = dataUrl[1];
+console.log(formData);
+// formData = dataUrl[1].split('&')
 
+formDataArray = formData.split('&');
+console.log(formDataArray);
 
-// modal code
-document.querySelectorAll(".clicked-modal").forEach(click => {
-    click.addEventListener("click", (e) => {
-        e.preventDefault();
-        const clickedModal = e.target.getAttribute("href").substring(1);
-        document.getElementById(clickedModal).showModal();
-    });
+formDataArray.forEach((data) => {
+    dataValue = data.split('=');
+    // console.log(dataValue);
+    if (dataValue[0] !== 'organization-title' && dataValue[0] !== 'membership' && dataValue[0] !== 'organization-description') {
+        if (dataValue[0] == 'email') {
+            dataValue[1] = dataValue[1].replace("%40", "@")
+        }
+        if (dataValue[0] == 'phone-number') {
+            dataValue[1] = dataValue[1].replace("%2B", "+")
+        }
+        if (dataValue[0] == 'time-stamp') {
+            dataValue[0] = 'Time of Submission';
+            dataValue[1] = dataValue[1].replace("%2F", "/");
+            dataValue[1] = dataValue[1].replace("%2F", "/");
+            dataValue[1] = dataValue[1].replace("%2C", ",");
+            dataValue[1] = dataValue[1].replace("+", " ");
+            dataValue[1] = dataValue[1].replace("%3A", ":");
+            dataValue[1] = dataValue[1].replace("%3A", ":");
+            dataValue[1] = dataValue[1].replace("+", " ");
+        }
+        const element = document.createElement('p');
+        element.innerHTML = `<p><strong>${capitalize(dataValue[0])}:</strong> ${dataValue[1]}</p>`
+        results.appendChild(element);
+    }
 });
 
-document.querySelectorAll(".close-modal").forEach(modal => {
-    modal.addEventListener("click", (e) => {
-        e.target.closest("dialog").close();
-    });
-});
-
+// Capitalize each word
+function capitalize(str) {
+    return str.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
 
 // Footer code
+let currentDate = new Date();
 let lastModified = new Date(document.lastModified);
 
 let year = document.getElementById("currentyear");
