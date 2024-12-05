f23b8819f934fd728743891c61fb835d0984e2fa
Natnael31 <end24002byui.edu>
2024-12-05 22:13:21 +0300

    discover update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -52,6 +52,46 @@ discover.addEventListener("click", () => {
 })
 
 
+// Main code
+
+let pageVistCount = Number(localStorage.getItem('visit'));
+localStorage.setItem('visit', ++pageVistCount);
+console.log(pageVistCount)
+
+// localStorage.removeItem('visit');
+
+
+if (pageVistCount == 1) {
+    alert('Welcome! Let us know if you have any questions.')
+    var firstDate = Number(localStorage.getItem('firstDate'));
+    localStorage.setItem('firstDate', Date.now());
+
+} else {
+    firstDate = Number(localStorage.getItem('firstDate'));
+    let pastDate = new Date(firstDate);
+    let pastDate_day = pastDate.getDate();
+    console.log(pastDate_day);
+
+    let currentDate = Number(localStorage.getItem('currentDate'));
+    localStorage.setItem('currentDate', Date.now());
+
+    currentDate = Number(localStorage.getItem('currentDate'));
+    let presentDate = new Date(currentDate);
+    let presentDate_day = presentDate.getDate();
+    console.log(presentDate_day);
+
+    if (presentDate_day - pastDate_day == 0) {
+        alert('Back so soon! Awesome!');
+    } else if (presentDate_day - pastDate_day == 1) {
+        alert('You last visited one day ago');
+    } else {
+        alert(`You last visited ${presentDate_day - pastDate_day} days ago`);
+    }
+
+
+    // localStorage.removeItem('firstDate')
+
+}
 
 
 
