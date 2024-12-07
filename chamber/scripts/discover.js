dc0176b27020970938de078801406c4f6b0dc45c
Natnael31 <end24002byui.edu>
2024-12-07 10:21:59 +0300

    js

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -54,6 +54,12 @@ discover.addEventListener("click", () => {
 
 // Main code
 
+const display = document.getElementById('display-message');
+const message = document.createElement('h3');
+
+display.classList.add('card-container')
+
+
 let pageVistCount = Number(localStorage.getItem('visit'));
 localStorage.setItem('visit', ++pageVistCount);
 console.log(pageVistCount)
@@ -62,37 +68,46 @@ console.log(pageVistCount)
 
 
 if (pageVistCount == 1) {
-    alert('Welcome! Let us know if you have any questions.')
+    // setTimeout(() => {
+    //     alert('Welcome! Let us know if you have any questions.')
+    // }, 500);
+    message.textContent = 'Welcome! Let us know if you have any questions.';
+    display.append(message);
+
     var firstDate = Number(localStorage.getItem('firstDate'));
     localStorage.setItem('firstDate', Math.floor(Date.now() / 3600000));
-
     // localStorage.removeItem('firstDate')
     // localStorage.removeItem('currentDate');
-
 } else {
     firstDate = Number(localStorage.getItem('firstDate'));
-    // let pastDate = new Date(firstDate);
-    // let pastDate_day = pastDate.getDate();
     console.log(firstDate);
 
     let currentDate = Number(localStorage.getItem('currentDate'));
     localStorage.setItem('currentDate', Math.floor(Date.now() / 3600000));
 
     currentDate = Number(localStorage.getItem('currentDate'));
-    // let presentDate = new Date(currentDate);
-    // let presentDate_day = presentDate.getDate();
     console.log(currentDate);
 
     if (currentDate - firstDate < 24) {
-        setTimeout(() => {
-            alert('Back so soon! Awesome!');
-        }, 500);
+        // setTimeout(() => {
+        //     alert('Back so soon! Awesome!');
+        // }, 500);
+        message.textContent = 'Back so soon! Awesome!';
+        display.append(message);
 
     } else if (currentDate - firstDate == 24) {
-        alert('You last visited one day ago');
+        // setTimeout(() => {
+        //     alert('You last visited one day ago');
+        // }, 500);
+        message.textContent = 'You last visited one day ago.';
+        display.append(message);
         localStorage.setItem('firstDate', Math.floor(Date.now() / 3600000));
     } else {
-        alert(`You last visited ${(currentDate - firstDate) / 24} days ago`);
+        // setTimeout(() => {
+        //     alert(`You last visited ${(currentDate - firstDate) / 24} days ago`);
+        // }, 500);
+        message.textContent = `You last visited ${(currentDate - firstDate) / 24} days ago`;
+        display.append(message);
         localStorage.setItem('firstDate', Math.floor(Date.now() / 3600000));
     }
 
