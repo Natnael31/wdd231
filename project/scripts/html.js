d046869f27a7893c050d79b4bdab2ab691b1d316
Natnael31 <end24002byui.edu>
2024-12-10 14:25:01 +0300

    final project

---
+++ project/scripts/html.js
@@ -0,0 +1,61 @@
+// header code
+
+// Burger code
+const ham = document.querySelector('nav');
+const list = document.getElementById('navigation');
+
+
+ham.addEventListener('click', () => {
+    list.classList.toggle('show');
+    ham.classList.toggle('show');
+})
+
+
+// header way finding code
+const home = document.getElementById('home');
+const html = document.getElementById('html');
+const css = document.getElementById('css');
+const js = document.getElementById('js');
+const register = document.getElementById('register');
+
+html.classList.add('active');
+home.addEventListener("click", () => {
+    home.classList.add('active');
+    html.classList.remove('active');
+    css.classList.remove('active');
+    js.classList.remove('active');
+    register.classList.remove('active');
+})
+
+html.addEventListener("click", () => {
+    html.classList.add('active');
+    home.classList.remove('active');
+    css.classList.remove('active');
+    js.classList.remove('active');
+    register.classList.remove('active');
+})
+
+css.addEventListener("click", () => {
+    css.classList.add('active');
+    home.classList.remove('active');
+    html.classList.remove('active');
+    js.classList.remove('active');
+    register.classList.remove('active');
+})
+
+js.addEventListener("click", () => {
+    js.classList.add('active');
+    home.classList.remove('active');
+    html.classList.remove('active');
+    css.classList.remove('active');
+    register.classList.remove('active');
+})
+
+register.addEventListener("click", () => {
+    register.classList.add('active');
+    home.classList.remove('active');
+    html.classList.remove('active');
+    css.classList.remove('active');
+    js.classList.remove('active')
+})
+
