69292bda3e51d96bc730272c32812f77985f051d
Natnael31 <end24002byui.edu>
2024-12-10 19:05:20 +0300

    footer

--- project/scripts/js.js
+++ project/scripts/js.js
@@ -59,3 +59,20 @@ register.addEventListener("click", () => {
     js.classList.remove('active')
 })
 
+
+
+
+
+
+
+
+
+//  Footer code
+let currentDate = new Date();
+let lastModified = new Date(document.lastModified);
+
+let year = document.getElementById("currentyear");
+year.textContent = `©${currentDate.getFullYear()} `;
+
+let date = document.getElementById("lastModified");
+date.textContent = `Last Modification: ${lastModified.getMonth() + 1} /${lastModified.getDate()}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes()}:${lastModified.getSeconds()} `
